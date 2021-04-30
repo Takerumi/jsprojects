87749e73c4d4e2f1bee5b551f43a11e58d862970
Borman Karlovich <mrgabrielripper@gmail.com>
2021-04-30 17:28:00 +0600

    Few new conspects

---
+++ js/arrMethods.js
@@ -0,0 +1,23 @@
+// const arr = [18, 2, 13, 61, 8];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = prompt('Введите товары через запятую', '');
+// const products = str.split(', '); //разделяем строку в массив
+// products.sort(); //сортировка строк
+// console.log(products.join('; ')); //склеиваем массив
